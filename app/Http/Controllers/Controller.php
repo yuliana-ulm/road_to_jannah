cb64ede34179bbd24e08a1d12a28863166869d71
yuliana-ulm <yana84594@gmail.com>
2023-05-06 21:52:33 +0700

    Session

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use GuzzleHttp\Psr7\Request;
+use Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
-    //menampilkan isi session 
-    public function tampilkansession(Request $request)
+    // use AuthorizesRequests, ValidatesRequests;
+    // //menampilkan isi session
+    // public function tampilkansession(Request $request)
 }
