5abb754bfd6db32b0e14767625d049293e3419a4
dzakihafizhikandangan <112626822+dzakihafizhikandangan@users.noreply.github.com>
2023-06-10 14:34:30 +0800

    Add files via upload

---
+++ public/fiqih - Sholat Jum'at.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>RoadToJannah.Or.Id - dakwah gasan banua</title>
+<link href='https://fonts.googleapis.com/css?family=Noto Sans Saurashtra' rel='stylesheet'>
+<style>
+    body {
+        font-family: 'Noto Sans Saurashtra';
+    }
+    .body{
+        height: 100%;
+        width: 100%;
+    }
+    /* =======header=============== */
+    .hnavbar{
+        border-bottom: solid grey 1px;
+        padding-left: 550px;
+        display: flex;
+        gap: 80px;
+    }
+    .ntulisan{
+        color: #4a7c12;
+        padding: 15px;
+        margin-top: 0px;
+        margin-bottom: 0px;
+        cursor: pointer;
+        font-weight: bolder;
+    }
+    a:link {
+        text-decoration: none;
+    }
+    .ntulisan:hover{
+        opacity: 0.6;
+        border-bottom: solid #47c200 1px;
+        background-color: #A8F385;
+    }
+    .header{
+        background-color: #bdde98;
+        padding-top: 10px;
+        padding-left: 10px;
+        padding-bottom: 10px;
+    }
+    .gambar{
+        width: 250px;
+        height: 50px;
+    }
+    .tanggal{
+        background-color: #fffde7;;
+        width: 100%;
+        height: 60px;
+    }
+    .flex-ikon{
+        padding-top: 10px;
+        padding-left: 10px;
+        padding-bottom: 10px;
+        display: flex;
+        gap: 20px;
+    }
+    .ikon{
+        width: 35px;
+        height: 35px;
+        cursor: pointer;
+        opacity: 0.6;
+    }
+    .flex-luar{
+        display: flex;
+    }
+    .date{
+        margin-left: 700px;
+        opacity: 0.6;
+    }
+    /*=============================*/
+
+     /* =======Konten=============== */
+    .konten{
+        width: 950px;
+        height: auto;
+        margin-left: 450px;
+        margin-top: 50px;
+        font-size: 15px;
+    }
+    .judul-konten{
+        border-bottom: solid grey 2px;
+        font-size: 40px;
+        padding-bottom: 30px;
+    }
+    .logo-konten{
+        margin-top: 9px;
+        width: 30px;
+        height: 30px;
+        opacity: 0.6;
+    }
+    .admin-konten{
+        display: flex;
+        gap: 10px;
+    }
+    .keterangan-konten{
+        opacity: 0.6;
+    }
+    .gambar-konten{
+        margin-top: 20px;
+        margin-bottom: 50px;
+        width: 950px;
+        height: 500px;
+    }
+    /*=============================*/
+
+    /* =======footer=============== */
+    .footer{
+        background-color: #bdde98;
+        margin-top: 70px;
+        width: 100%;
+        height: 150px;
+    }
+    .judul-footer{
+        font-weight: bold;
+        display: flex;
+        gap: 250px;
+        padding-left: 85px;
+        color: #4a7c12;
+    }
+    .isi-judul-footer{
+        border-bottom: solid #4a7c12 2px;
+        padding-bottom: 10px;
+        width: 350px;
+    }
+    .isi-footer{
+        display: flex;
+        padding-left: 85px;
+    }
+    .isi-footer-konten-a{
+        width: auto;
+        cursor: pointer;
+    }
+    .isi-footer-konten-a:hover{
+        opacity: 0.6;
+    }
+    .isi-footer-konten-b{
+        margin-left: 470px;
+        width: auto;
+        cursor: pointer;
+    }
+    .isi-footer-konten-b:hover{
+        opacity: 0.6;
+    }
+    .isi-footer-konten-c{
+        margin-left: 400px;
+        width: auto;
+        cursor: pointer;
+    }
+    .isi-footer-konten-c:hover{
+        opacity: 0.6;
+    }
+    /*=============================*/
+</style>
+</head>
+<body>
+    <div class="body">
+        <div class="tanggal">
+            <div class="flex-luar">
+                <div class="flex-ikon">
+                    <img src ="img/fb.png" class="ikon" width="100%" height="100%">
+                    <img src ="img/ig.png" class="ikon" width="100%" height="100%">
+                    <img src ="img/yt.png" class="ikon" width="100%" height="100%">
+                </div>
+                <p class="date" id="date"></p>
+            </div>
+        </div>
+        <div class="header">
+            <div class="gambar">
+                <img src ="img/typo.png" width="100%" height="100%">
+            </div>
+        </div>
+        <div class="hnavbar">
+            <a href="dashboard - fiqih.php"><p class="ntulisan">Fiqih</p></a>
+            <p class="ntulisan">Manhaj</p>
+            <p class="ntulisan">Aqidah</p>
+            <p class="ntulisan">Sejarah</p>
+            <p class="ntulisan">Tentang Website  </p>
+        </div>
+        <div class="konten">
+            <h1 class="judul-konten">Sambahyang Jum'at</h1>
+            <div class="admin-konten">
+                <div class="logo-konten">
+                    <img src ="img/akun.png" width="100%" height="100%">
+                </div>
+                <div class="keterangan-konten">
+                    <p>oleh <b>Yuliana -</b> 6 Juni 2023 Waktu Baca:7 menit</p>
+                </div>
+            </div>
+            <div class="gambar-konten">
+                <img src ="img/sholat jumat.jpg" width="100%" height="100%">
+            </div>
+            <p>
+                <b>Tanya :</b> apa nang dimaksud lawan sumbahyang jumat?
+            </p>
+            <p>
+                <b>Jawab :</b> Sumbahyang jumat merupakan sumbahyang yang digawi diwayah hari jumat sama nangkaya masuknya waktu sumbahyang dzuhur yang digawi dua rakaat imbah dua khutbah. Sumbahyang Jumat merupakan aktivitas ibadah wajib yang digawi secara berjama'ah bagi lalakian muslim setiap hari Jumat yang menggantikan sumbahyang dzuhur.
+            </p>
+            <p>
+                Sumbahyang Jumat adalah sumbahyang  yang diwajibkan pada waktu hari Jumat dan lain sumbahyang dzuhur kaya nang dipahami oleh sebagian manusia. Tidak diperbolehkan bagi seorang muslim mukallaf meninggalkan sumbahyang Jumat tanpa adanya udzur syar'i.</p>
+            <p>
+                <b>Tanya :</b> Tabila sumbahyang jumat digawi?
+            </p>
+            <p>
+                <b>Jawab :</b> Wayah sumbahyang Jumat, Kabanyakan sahabat dan tabi’in satuju bahwa wayah sumbahyang jumat sama lawan wayah sumbahyang dzuhur. 
+            </p>
+            Hal ini berdasarkan hadits dari Anas bin Malik RA yang diriwayatkan Abu Dawud dan Abu Ya’la: <br><br>
+            كَانَ رَسُولُ اللَّهِ -صلى الله عليه وسلم- يُصَلِّى الْجُمُعَةَ إِذَا مَالَتِ الشَّمْسُ       
+
+            <br><br> "Nabi shallallahu ‘alaihi wasallam mengerjakan Shalat Jumat apabila matahari telah tergelincir"
+            <p>
+                <b>Tanya :</b> Kaya apa tata cara menggawi sumbahyang jum'at ?
+            </p>
+            <p>
+                <b>Jawab :</b>Tata Cara Shalat Jumat Shalat jum'at disyariatkan untuk dikerjakan secara berjamaah, tidak sah jika sendirian. Adapun jumlah minimal jamaahnya, para ulama berbeda pendapat. Ibnu Hajar Al Asqalani menjelaskan dalam Fathul Bari’ bahwa ada 15 pendapat para ulama. Pendapat paling kuat menurut Sayyid Sabiq adalah, ia telah sah meskipun hanya diikuti dua orang atau lebih. Tempatnya boleh di kota maupun di desa, di dalam bangunan maupun di lapangan, namun yang paling utama adalah di masjid.
+            </p>
+            <p>
+                Shalat jum'at dilaksanakan dua rakaat dengan dipimpin oleh imam. Secara ringkas, tata caranya adalah sebagai berikut:
+                <ol>
+                <li>Niat</li>
+                <li>Takbiratul ihram, diikuti dengan Doa Iftitah</li>
+                <li>Membaca Surat Al Fatihah</li>
+                <li>Membaca surat dari Al Qur’an, disunnahkan Al A’la</li>
+                <li>Ruku’ dengan tuma’ninah</li>
+                <li>I’tidal dengan tuma’ninah</li>
+                <li>Sujud dengan tuma’ninah</li>
+                <li>Duduk di antara dua sujud dengan tuma’ninah</li>
+                <li>Sujud kedua dengan tuma’ninah</li>
+                <li>Berdiri lagi untuk menunaikan rakaat kedua</li>
+                <li>Membaca surat Al Fatihah</li>
+                <li>Membaca surat dari Al Qur’an, disunnahkan Al Ghatsiyah</li>
+                <li>Ruku’ dengan tuma’ninah</li>
+                <li>I’tidal dengan tuma’ninah</li>
+                <li>Sujud dengan tuma’ninah</li>
+                <li>Duduk di antara dua sujud dengan tuma’ninah</li>
+                <li>Sujud kedua dengan tuma’ninah</li>
+                <li>Tahiyat akhir dengan tuma’ninah</li>
+                <li>Salam</li>
+                </ol>
+            </p>
+            <p>
+                <b>Tanya :</b> kayapa niat sagan sumbahyang jum'at?
+            </p>
+            <p>
+                <b>Jawab :</b> sagan niatnya kayaini</p>
+                Berikut ini adalah lafadz niat shalat jumat bagi makmum:<br><br>
+
+            أُصَلِّي فَرْضَ الْجُمْعَةِ مَأْمُومًا لِلهِ تَعَالَى
+
+            <br><br>"Aku niat sholat Jumat dua rakaat sebagai imam karena Allah Ta’ala"
+
+            Adapun lafadz niat shalat jum'at bagi imam adalah sebagai berikut:<br><br>
+
+            أُصَلِّي فَرْضَ الْجُمْعَةِ إِمَامًا لِلهِ تَعَالَى
+
+            <br><br>"Saya shalat jumat sebagai imam karena Allah Ta'ala"
+            <p>
+                <b>Tanya :</b> Apa Utamanya bila sumbahyang jum'at?
+            </p>
+            <p>
+                <b>Jawab :</b> Ada banyak keutamaannya, ini cuntuhnya:
+            </p>
+            1. Penghapus Dosa
+            Di antara keutamaan shalat jum'at adalah sebagai penghapus dosa antara dua Jumat. Hadits dari Abu Hurairah RA yang:<br><br>
+
+            الصَّلاَةُ الْخَمْسُ وَالْجُمُعَةُ إِلَى الْجُمُعَةِ كَفَّارَةٌ لِمَا بَيْنَهُنَّ مَا لَمْ تُغْشَ الْكَبَائِرُ
+
+            <br><br>“Sholat lima waktu dan Jumat yang satu ke Jumat yang berikutnya dapat menghapuskan dosa di antara keduanya selama tidak dilakukan dosa besar.”
+
+            2. Pahala Besar
+            Keutamaan shalat jumat lainnya adalah memperoleh pahala besar. Pahala ini senilai pahala qurban yang disesuaikan dengan waktu kedatangannya di masjid. Sebagaimana hadits yang diriwayatkan oleh Imam Bukhari dan Imam Muslim:<br><br>
+
+            مَنْ اغْتَسَلَ يَوْمَ الْجُمُعَةِ غُسْلَ الْجَنَابَةِ ثُمَّ رَاحَ فَكَأَنَّمَا قَرَّبَ بَدَنَةً وَمَنْ رَاحَ فِي السَّاعَةِ الثَّانِيَةِ فَكَأَنَّمَا قَرَّبَ بَقَرَةً وَمَنْ رَاحَ فِي السَّاعَةِ الثَّالِثَةِ فَكَأَنَّمَا قَرَّبَ كَبْشًا أَقْرَنَ وَمَنْ رَاحَ فِي السَّاعَةِ الرَّابِعَةِ فَكَأَنَّمَا قَرَّبَ دَجَاجَةً وَمَنْ رَاحَ فِي السَّاعَةِ الْخَامِسَةِ فَكَأَنَّمَا قَرَّبَ بَيْضَةً فَإِذَا خَرَجَ الْإِمَامُ حَضَرَتْ الْمَلَائِكَةُ يَسْتَمِعُونَ الذِّكْرَ
+
+            <br><br>"Barangsiapa mandi pada hari Jumat sebagaimana mandi jinabat, lalu berangkat menuju masjid, maka dia seolah berqurban dengan seekor unta. Barangsiapa yang datang pada saat kedua maka dia seolah berqurban dengan seekor sapi. Barangsiapa yang datang pada saat ketiga maka dia seolah berqurban dengan seekor kambing yang bertanduk. Barangsiapa yang datang pada saat keempat maka dia seolah berqurban dengan seekor ayam. Dan barangsiapa yang datang pada sat kelima maka dia seolah berqurban dengan sebutir telur. Dan apabila imam sudah keluar (menyampaikan khutbah), maka para malaikat hadir mendengarkan dzikir (khutbah tersebut)"
+
+            3. Seperti Ibadah Setahun
+            Keutamaan shalat jum'at yang sempurna yaitu yang diawali mandi, kemudian setiap langkahnya senilai puasa dan shalat setahun. Sebagaimana hadits yang diriwayatkan oleh Imam Tirmidzi:<br><br>
+
+            مَنْ اغْتَسَلَ يَوْمَ الْجُمُعَةِ وَغَسَّلَ ، وَبَكَّرَ وَابْتَكَرَ ، وَدَنَا وَاسْتَمَعَ وَأَنْصَتَ ، كَانَ لَهُ بِكُلِّ خُطْوَةٍ يَخْطُوهَا أَجْرُ سَنَةٍ صِيَامُهَا وَقِيَامُهَا
+
+            <br><br>"Barangsiapa yang mandi pada hari Jumat lalu ia bergegas pergi (ke masjid), mendapati khutbah mendengar dan memperhatikan, maka setiap langkah kakinya terhitung seperti puasa dan shalat setahun"
+            <p>***</p>
+            <b>
+            <p>Alih Bahasa : Yuliana</p>
+            <p>Sumber : www.laduni.id/</p>
+            <p>Dipublikasikan kembali oleh www.rtj.or.id</p>
+            </b>
+        </div>
+        <div class="footer">
+            <div class="judul-footer">
+                <p class="isi-judul-footer">Siapa Kami</p>
+                <p class="isi-judul-footer">Media Partner</p>
+                <p class="isi-judul-footer">Road To Jannah</p>
+            </div>
+            <div class="isi-footer">
+                <p class="isi-footer-konten-a">Tentang Website</p>
+                <p class="isi-footer-konten-b">Pendidikan Komputer FKIP</p>
+                <p class="isi-footer-konten-c">Universitas Lambung Mangkurat</p>
+            </div>
+        </div>
+    </div>
+<script>
+    /*===========TANGGAL==================*/
+    var d = new Date();
+    var month = d.getMonth() + 1;
+    var date = d.getDate();
+    var year = d.getFullYear();
+    var fullDate = date + " - " + month + " - " + year;
+    document.getElementById("date").innerHTML = fullDate;
+    /*=============================*/
+</script>
+</body>
+</html>
